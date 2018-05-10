0d955e100fce0602fd36259e2f36dc374cb6dc5b
Huanle Hu <huanlehu@Huanles-MacBook-Pro.local>
2018-05-10 23:09:32 +0900

    add source files

---
+++ src/main/java/com/huanle/mybatis/dao/IUser.java
@@ -0,0 +1,20 @@
+package com.huanle.mybatis.dao;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+
+import com.huanle.mybatis.models.User;
+
+public interface IUser {
+	//@Select("select * from user where id= #{id}")
+    //public User getUserByID(int id);
+    public List<User> getUserList();
+
+    public void insertUser(User user);
+
+    public void updateUser(User user);
+
+    public void deleteUser(int userId);
+
+    public User getUser(int id);
+}
